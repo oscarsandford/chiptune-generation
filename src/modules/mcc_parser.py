c78f209eafbecf580f4741da434b37936cc42e93
Colson Demedeiros <cdemedeiros@uvic.ca>
2022-02-27 22:03:30 -0800

    added code for the extract_midi_info method in mcc_parser.py

--- src/modules/mcc_parser.py
+++ src/modules/mcc_parser.py
@@ -9,7 +9,7 @@ def open_midi(filepath:str) -> MidiFile:
 	return MidiFile(filepath, clip=True)
 
 
-def extract_midi_info(meta_messages: list) -> dict:
+def extract_midi_info(meta_messages: list) -> list:
 	"""
 	Input the first track of a MIDI file containing meta messages
 	with key info such as timestamps for tempo change.
@@ -17,21 +17,32 @@ def extract_midi_info(meta_messages: list) -> dict:
 
 	TODO: implement this.
 	"""
-	print(meta_messages)
-	return {}
+
+	MidiInfo = []
+    for msg in meta_messages:
+        if msg.type == "time_signature":
+            info = (msg.type, msg.numerator, msg.denominator)
+            MidiInfo.append(info)
+        if msg.type == "key_signature":
+            info = (msg.type, msg.key)
+            MidiInfo.append(info)
+        if msg.type == 'set_tempo':
+            info = (msg.type, msg.tempo, msg.time)
+            MidiInfo.append(info)
+	return MidiInfo
 
 
 def extract_midi_tracks(mid_tracks:list) -> list:
 	"""
-	Iterate over a list of tracks and create a list for each 
+	Iterate over a list of tracks and create a list for each
 	track containing the notes in each track, preserving the order.
-	
+
 	Each note is stored as a 4-tuple:
 		(
 			note: int, the note key on a piano
 			velocity: int, the strength of the key where 0 means the note is off, or a rest
-			time: int, the wait time between the last and current note or operation, so the 
-				duration of a note is the sum of time in between the two nearest messages 
+			time: int, the wait time between the last and current note or operation, so the
+				duration of a note is the sum of time in between the two nearest messages
 				containing the same note
 			on/off: bool, essentially whether this note is off or not
 		)
@@ -39,7 +50,7 @@ def extract_midi_tracks(mid_tracks:list) -> list:
 	notes_tracks = []
 	for track in mid_tracks:
 		track_notes = []
-		# TODO: It will be worth adding more data to the track. 
+		# TODO: It will be worth adding more data to the track.
 		# For example, the type of instrument that is playing, or a specific tempo.
 		for msg in track:
 			# Only work on messages that describe notes.
