4115bac4315e91291a33feb2c042bc12db545feb
Oscar Sandford <oscarsandford@gmail.com>
2022-03-10 20:35:31 -0800

    Add KMarkov class concept for higher-order precision modelling.
    KMarkov is still rudimentary, and has been minimally tested. Tests so far show that the algorithm suffers in generating large amounts of samples for higher-order processes. The higher the order, the lower the number of samples before slowdown starts to occur.

--- src/modules/mcc_markov.py
+++ src/modules/mcc_markov.py
@@ -5,13 +5,16 @@
 import numpy as np
 
 """
-TODO: extend MarkovModel class to allow modelling of higher-order Markov processes.
-This should lead to better results in pattern prediction.
+TODO: 
+
+Clean up KMarkov, and MarkovModel in terms of code and documentation.
+
+KMarkov is still rudimentary, but seems to outclass MarkovModel off the bat.
+However, it is much slower. Keep both around just in case.
 """
 
-
 class MarkovModel:
-	def __init__(self, states:set=None, transmat=None, order:int=1, epsilon:float=0.0):
+	def __init__(self, states:set=None, transmat=None, epsilon:float=0.0):
 		"""
 		A class for constructing first-order Markov models.
 				
@@ -29,10 +32,6 @@ class MarkovModel:
 		It is important to note that the states can be any hashable type that can be put 
 		into a set, not just strings.
 
-		The `order` parameter can be set to increase the number of previous states that are 
-		considered when predicting the next state. This will make the transition matrix more 
-		complex as a result. Use fit() to build the model for higher-order processes.
-
 		The `epsilon` parameter can be set on the range [0.0, 1.0] in order to bias a greedy 
 		decision making choice in prediction. 
 			If epsilon=1.0, the most probable transition will always be chosen. Careful, as this 
@@ -40,18 +39,18 @@ class MarkovModel:
 			If epsilon=0.0 (by default), the transition probabilities will be weighted accordingly. 
 			If epsilon=0.5, there will be a 50% chance of taking the most probable action, and a 
 				50% chance of making a weighted choice between the other actions.
+		
+		This was a terrible idea. Only mess with epsilon if you're feeling chaotic.
 		"""
 		self.states = states
 		self.transmat = transmat
-		self.order = order
 		self.epsilon = epsilon
 
 		if not(transmat is None or states is None):
-			assert order == 1, "MCC: higher-order model should not be initialized with transmat."
 			assert len(transmat.shape) == 2 and transmat.shape[0] == transmat.shape[1], "MCC: transmat of inadequate shape."
 			assert len(states) == transmat.shape[0], "MCC: transmat row dimension does not match state set cardinality."
 			# Assure that the set of states is a set of unique elements in the case a list is passed by accident.
-			self.states = set(states)
+			self.states = list(set(states))
 			# Define transmat indices for future state-index lookups if model provided.
 			self._transmat_idxs = dict((s,i) for i,s in enumerate(states))
 		else:
@@ -61,22 +60,29 @@ class MarkovModel:
 		assert 0 <= epsilon <= 1.0, "MCC: epsilon must be in range [0.0, 1.0]."
 
 
-	def fit(self, event:list):
+	def fit(self, event: list or str):
 		"""
-		Create a model by fitting a given event as a ordered list 
-		of states. Devise transmat based on state transitions.
+		Create a model by fitting a given event as either:
+			an ordered list of states OR 
+			a string of states separated by commas. 
+		Devise transmat based on state transitions.
 		"""
-		self.states = set(event)
+		if type(event) is str:
+			assert "," in event, "MCC: Separate states in string representation with commas."
+			self.states = self.event.split(",")
+		else:
+			self.states = list(set(event))
+		
 		self.transmat = np.zeros((len(self.states),len(self.states)))
 
 		# Go through the event and figure out how many 
 		# times state s goes to state s' for all s.
 		transcounts = {}
-		for i in range(len(event)-1):
-			if not (event[i], event[i+1]) in transcounts:
-				transcounts[(event[i], event[i+1])] = 1
+		for i in range(1, len(event)):
+			if not (event[i-1], event[i]) in transcounts:
+				transcounts[(event[i-1], event[i])] = 1
 			else:
-				transcounts[(event[i], event[i+1])] += 1
+				transcounts[(event[i-1], event[i])] += 1
 		
 		# For all states s, populate the transition matrix with 
 		# probabilities of s transitioning to itself or any of 
@@ -128,3 +134,117 @@ class MarkovModel:
 	def __repr__(self) -> str:
 		return "States:\n" + str(self.states) + \
 		"\nTransition matrix:\n" + str(self.transmat)
+
+
+
+
+class KMarkov():
+	def __init__(self, order:int, states:set=None):
+		"""
+		The `order` parameter is set to increase the number of previous states that are 
+		considered when predicting the next state. This will make the transition matrix more 
+		complex as a result. Use fit() to build the model for higher-order processes.
+		"""
+		self.k = order
+		# Transition probabilities are stored in a dictionary for more efficient and flexible storage.
+		# The format is 
+		# TP[string of prior states] = (next state, probability to do this transition to next state)
+		self.TP = {}
+		if not states is None:
+			self.states = list(set(states))
+
+
+	def fit(self, event:list or str):
+		"""
+		Do fitment.
+
+		TODO: this docstring needs more work. Elaborate on the workings.
+		"""
+		if type(event) is str:
+			assert "," in event, "MCC: Separate states in string representation with commas."
+			event = event.split(",")
+		
+		assert len(event) > self.k, f"MCC: Cannot fit with order {self.k} to event of size {len(event)}."
+		self.states = list(set(event))
+
+		# Create counts of how many times a sequence of o states results in a new state s.
+		for i in range(len(event)-self.k):
+
+			priors = ",".join(event[i:i+self.k])
+			next = event[i+self.k]
+
+			# Case 1: prior states already recorded and next state is indexable and incrementable.
+			if priors in self.TP and next in self.TP[priors]:
+				self.TP[priors][next] += 1.0
+
+			# Case 2: prior states already recorded BUT next state is not yet included as possible outcome.
+			elif priors in self.TP:
+				self.TP[priors][next] = 1.0
+
+			# Case 3: prior states not recorded. Initialize them with next state as only possible outcome (so far).
+			else:
+				self.TP[priors] = {next: 1.0}
+
+		# Replace counts with probabilities of that selection 
+		# of prior states switching to the next state.
+		for priors in self.TP:
+			csum = sum(self.TP[priors].values())
+			for next in self.TP[priors]:
+				self.TP[priors][next] /= csum
+
+		# for p in self.TP:
+		# 	print(p ,"-->" , self.TP[p])
+
+
+	def predict(self, samples:int, init_states=None) -> list:
+		"""
+		Generate a given number of samples from the model.
+
+		TODO: this docstring needs work. Elaborate on the why and how.
+		"""
+		assert not(self.states is None or self.TP is None), "MCC: cannot predict without model."
+
+		# Grab a random set of k consecutive states that will definitely have a next state.
+		preds = str(np.random.choice(list(self.TP.keys()))).split(",")
+
+		for _ in range(samples):
+
+			# Predict the next state given the current predictions.
+			# If we find at least one exact match, we choose the next state 
+			# probabilistically, depending on the probability distribution 
+			# among the possible next states.
+			# If we don't find an exact match, look for the next shortest 
+			# match, starting by reducing the size from the start.
+			# i.e.
+			# predictions[-o:] = [c,d] (last o elements we predict with)
+			# ----
+			# don't match at all: e.g. [a,c,d,b], [c,d,a], ...
+			# ----
+			# prev:[a,b,c,d] -> next:[e]
+			# reduce
+			# prev:[b,c,d] -> next:[e]
+			# reduce again
+			# prev:[c,d] -> next:[e]
+			# => next state is [e]
+
+			# Only consider the k most recent states visited.
+			priors = ",".join(preds[-self.k:])
+			# print("Init priors:", priors)
+
+			while not priors in self.TP:
+				# Reduce from beginning.
+				priors = ",".join(priors.split(",")[1:])
+				# print(" reduced priors:", priors)
+
+			# print("end priors:", priors)
+			# Now TP is safe to access. Probabilistically decided which next 
+			# state to return.
+			# print("keys: ", list(self.TP[priors].keys()))
+			# print("vals: ", list(self.TP[priors].values()))
+
+			next = str(np.random.choice(list(self.TP[priors].keys()), p=list(self.TP[priors].values())))
+			# print("next:", next)
+
+			preds.append(next)
+		
+		return preds
