3df0c9d5d5ef03aff4bc9fa3b5d42cddebe5ecce
Jae Park <jaepark052@gmail.com>
2022-04-03 20:29:30 -0700

    fixed filepath

--- src/modules/mcc_builder.py
+++ src/modules/mcc_builder.py
@@ -32,4 +32,4 @@ def export_to_wav(track: list, srate: int, name):
 	Save the input as .wav file.
 	"""
 	assert len(track) > 0
-	write('../out/' + name + '.wav', srate, track.astype(np.uint8))
+	write('./out/' + name + '.wav', srate, track)
